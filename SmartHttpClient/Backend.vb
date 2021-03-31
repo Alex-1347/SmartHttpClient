51f46bbdad40cc532967a8960f134aae691abc3f
Alex-1347 <kharkov275@gmail.com>
2021-03-31 21:45:49 +0300

    first commit

---
+++ SmartHttpClient/Backend.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Text
+Imports Newtonsoft.Json
+
+Public Class SmartHttpClient
+    Public Shared Async Function [Get](URL As String) As Threading.Tasks.Task(Of HttpResponseMessage)
+        Using Client As New SSLHttpClient()
+            Using ServiceRequest As New HttpRequestMessage(HttpMethod.Get, URL)
+                Return Await Client.SendAsync(ServiceRequest)
+            End Using
+        End Using
+    End Function
+
+    Public Shared Async Function [Delete](URL As String) As Threading.Tasks.Task(Of HttpResponseMessage)
+        Using Client As New SSLHttpClient()
+            Using ServiceRequest As New HttpRequestMessage(HttpMethod.Get, URL)
+                Return Await Client.SendAsync(ServiceRequest)
+            End Using
+        End Using
+    End Function
+
+    Public Shared Async Function [Post](URL As String, DictionaryForConverter As Dictionary(Of String, String)) As Threading.Tasks.Task(Of HttpResponseMessage)
+        Using Client As New SSLHttpClient()
+            Client.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+            Client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=utf-8")
+            Using RequestContent As New StringContent(JsonConvert.SerializeObject(DictionaryForConverter), Encoding.UTF8, "application/json")
+                Return Await Client.PostAsync(URL, RequestContent)
+            End Using
+        End Using
+    End Function
+
+    Public Shared Async Function GetStreamToBase64String(URL As String) As Threading.Tasks.Task(Of Tuple(Of String, String))
+        Dim Response = Await [Get](URL)
+        If Response.IsSuccessStatusCode Then
+            Dim NewStream As New IO.MemoryStream
+            Dim PhotoStream As IO.Stream = Await Response.Content.ReadAsStreamAsync
+            PhotoStream.CopyTo(NewStream)
+            PhotoStream.Close()
+            Dim ByteBuff As Byte() = NewStream.ToArray
+            Return New Tuple(Of String, String)(Convert.ToBase64String(ByteBuff), "OK")
+        Else
+            Return New Tuple(Of String, String)("", Response.ReasonPhrase)
+        End If
+    End Function
+End Class
+
---
+++ SmartHttpClient/SSLHttpClient.vb
@@ -0,0 +1,47 @@
+﻿'Require setting in web.config
+'<configuration>
+'  <appSettings>
+'    <add key="AcceptAnySSLCertificate" value="true"/>
+' </appSettings>
+'...
+
+Public Class SSLHttpClient
+        Inherits System.Net.Http.HttpClient
+        Public Sub New()
+            MyBase.New
+            If CBool(System.Configuration.ConfigurationManager.AppSettings("AcceptAnySSLCertificate")) Then
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = Function(s As Object,
+                                                                                              cert As System.Security.Cryptography.X509Certificates.X509Certificate,
+                                                                                              chain As System.Security.Cryptography.X509Certificates.X509Chain,
+                                                                                              err As System.Net.Security.SslPolicyErrors)
+                                                                                         Return True
+                                                                                     End Function
+            End If
+        End Sub
+
+        Public Sub New(handler As System.Net.Http.HttpMessageHandler)
+            MyBase.New(handler)
+            If CBool(System.Configuration.ConfigurationManager.AppSettings("AcceptAnySSLCertificate")) Then
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = Function(s As Object,
+                                                                                              cert As System.Security.Cryptography.X509Certificates.X509Certificate,
+                                                                                              chain As System.Security.Cryptography.X509Certificates.X509Chain,
+                                                                                              err As System.Net.Security.SslPolicyErrors)
+                                                                                         Return True
+                                                                                     End Function
+            End If
+
+        End Sub
+
+        Public Sub New(handler As System.Net.Http.HttpMessageHandler, disposeHandler As Boolean)
+            MyBase.New(handler, disposeHandler)
+            If CBool(System.Configuration.ConfigurationManager.AppSettings("AcceptAnySSLCertificate")) Then
+                System.Net.ServicePointManager.ServerCertificateValidationCallback = Function(s As Object,
+                                                                                              cert As System.Security.Cryptography.X509Certificates.X509Certificate,
+                                                                                              chain As System.Security.Cryptography.X509Certificates.X509Chain,
+                                                                                              err As System.Net.Security.SslPolicyErrors)
+                                                                                         Return True
+                                                                                     End Function
+            End If
+        End Sub
+    End Class
+
